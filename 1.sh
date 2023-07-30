40ba631fa4b9b76e588e548e0204594148af5663
Shixun Wu <swu264@login11.chn>
2023-07-30 11:44:27 -0700

    update

--- 1.sh
+++ 1.sh
@@ -12,8 +12,8 @@ cd -
 cd kernel/ft_fft_batch/include/code_gen/
 python3 main.py
 cd -
-make clean; make V_FFT=1 P_FFT=0 FT=0 LOG=LOG_OFF GLOBAL=GLOBAL_OFF -j
-CUDA_VISIBLE_DEVICES=3 ./ft_fft_batch 15 128
+make clean; make V_FFT=0 P_FFT=1 FT=0 LOG=LOG_OFF GLOBAL=GLOBAL_OFF -j
+# CUDA_VISIBLE_DEVICES=3 ./ft_fft_batch 21 128
 # ./ft_fft_batch 9
 # make clean; make V_FFT=0 P_FFT=1 FT=0 LOG=LOG_OFF GLOBAL=GLOBAL_OFF -j
 # ./ft_fft 23 29 
--- sbatch.sh
+++ sbatch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH -A swu264
+#SBATCH -A m4271
 #SBATCH -C gpu
 #SBATCH -q shared
 #SBATCH -t 1:00:00
 #SBATCH -n 1
 #SBATCH --gpus-per-task=1
 
-export SLURM_CPU_BIND="cores"
+
 srun ./ft_fft_batch 21 128
