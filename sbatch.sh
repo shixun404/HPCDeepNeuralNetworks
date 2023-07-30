45d7a762c39718a688b69bee819b5e7c4982ddd3
Shixun Wu <swu264@login03.chn>
2023-07-30 16:56:30 -0700

    updpate

--- sbatch.sh
+++ sbatch.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #SBATCH -A m4271
 #SBATCH -C gpu
-#SBATCH -q shared
-#SBATCH -t 1:00:00
 #SBATCH -n 1
 #SBATCH --gpus-per-task=1
+#SBATCH --exclusive
 
 export SLURM_CPU_BIND="cores"
-srun ./ft_fft_batch 21 128
+srun ./ft_fft_batch 8 128
